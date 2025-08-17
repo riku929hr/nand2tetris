cc7d34d992ae20f5674cdedb43c3ff2d0362c616
riku929hr <33782267+riku929hr@users.noreply.github.com>
2025-08-17 22:44:53 +0900

    add symboltable

--- project6/assembler/main.go
+++ project6/assembler/main.go
@@ -10,8 +10,25 @@ import (
 )
 
 func main() {
+	// filepath from args
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: go run main.go <path_to_asm_file>")
+		return
+	}
+	filePath := os.Args[1]
+	if filePath == "" {
+		fmt.Println("Please provide a valid file path.")
+		return
+	}
+
+	// check if file exists
+	if _, err := os.Stat(filePath); os.IsNotExist(err) {
+		fmt.Printf("File does not exist: %s\n", filePath)
+		return
+	}
+
 	// read from file
-	content, err := os.ReadFile("sample.asm")
+	content, err := os.ReadFile(filePath)
 	if err != nil {
 		fmt.Println("Error reading file:", err)
 		return
